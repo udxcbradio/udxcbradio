5f680a8945904a0432c59ec4f5356d815204b735
udxcbradio <145147144+udxcbradio@users.noreply.github.com>
2023-09-15 12:50:17 -0400

    Add files via upload

---
+++ bs-getAllCaptionText-ea8d5cb5.js
@@ -0,0 +1,2 @@
+define("@widget/GALLERY/c/bs-getAllCaptionText-ea8d5cb5.js",["exports"],(function(t){"use strict";t.g=t=>{let e;const n=t.blocks;if(n.length>1){const t=[];n.forEach((e=>""===e.text?t.push("\n\n"):t.push(e.text))),e=t.join("")}else e=n[0].text;return e}})),"undefined"!=typeof window&&(window.global=window);
+//# sourceMappingURL=bs-getAllCaptionText-ea8d5cb5.js.map
